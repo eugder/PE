38d693c388869525ecf395b6780d5e4ebe287dcf
eugder <eugder@gmail.com>
2022-06-23 12:33:56 +0300

    First version commit

---
+++ Luke_Im_Your.py
@@ -0,0 +1,7 @@
+def relation_to_luke(name):
+    names = {"Darth Vader": "father",
+        "Leia": "sister",
+        "Han": "brother in law",
+        "R2D2": "droid",}
+    result = names[name]
+    return result
